ab4e797c363b34ef3f974b5014a9d9b454a99172
Piyush <piyushbharti7052@gmail.com>
2024-04-17 18:46:10 +0530

    headers Part created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div className='text-red-500'>
-      App
-    </div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path='/' element = {<Home />}/>
+      <Route path='/sign-in' element = {<SignIn />}/>
+      <Route path='/sign-up' element = {<SignOut />}/>
+      <Route path='/about' element = {<About />}/>
+      <Route path='/profile' element = {<Profile />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
